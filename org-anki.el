8f5aac4205cdc1f464c377de9265fcb6b46d41c0
eyeinsky <eyeinsky9@gmail.com>
2020-12-03 14:27:38 +0200

    Add package attributes and description

--- org-anki.el
+++ org-anki.el
@@ -1,9 +1,37 @@
-;; -*- mode: lisp-mode; eval: (paredit-mode) -*-
-;; anki for org-mode
+;;; org-anki.el --- synchronize org-mode entries to Anki
 
-(require 'request)
-(require 'org)
+;; URL: https://github.com/eyeinsky/org-anki
+;; Version: 0.0.1
+;; Author: Markus Läll <markus.l2ll@gmail.com>
+;; Keywords: outlines, flashcards, memory
+;; Package-Requires: ((emacs "24"))
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; Minor mode to synchronize org-mode entries to Anki via AnkiConnect.
+;;
+;; See https://github.com/eyeinsky/org-anki for more.
+
+
+;;; Code:
+
 (require 'json)
+(require 'org)
+(require 'request)
 
 ;; Constants
 
