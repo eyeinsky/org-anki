71707fcc279889a7f380fe5ba189d9e33b0ed077
Markus Läll <markus.l2ll@gmail.com>
2021-08-02 22:02:53 +0300

    Bump version, add changelog

--- org-anki.el
+++ org-anki.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2021 Markus Läll
 ;;
 ;; URL: https://github.com/eyeinsky/org-anki
-;; Version: 0.0.3
+;; Version: 0.0.4
 ;; Author: Markus Läll <markus.l2ll@gmail.com>
 ;; Keywords: outlines, flashcards, memory
 ;; Package-Requires: ((emacs "24.4") (request "0.3.2"))
