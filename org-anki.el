384f257c83b0c698e140c54938e81f75f4fb6fa0
Markus Läll <markus.l2ll@gmail.com>
2023-01-14 18:51:39 +0200

    Bump version to 2.0.0
    Just in case as the field format changed. Let's hope it just works.

--- org-anki.el
+++ org-anki.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2022 Markus Läll
 ;;
 ;; URL: https://github.com/eyeinsky/org-anki
-;; Version: 1.0.5
+;; Version: 2.0.0
 ;; Author: Markus Läll <markus.l2ll@gmail.com>
 ;; Keywords: outlines, flashcards, memory
 ;; Package-Requires: ((emacs "27.1") (request "0.3.2") (dash "2.17") (promise "1.1"))
