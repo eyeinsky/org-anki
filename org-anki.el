a7673b97e0961656a10e270ef8c271c18bc5b163
basaran <basaran@protonmail.com>
2023-06-16 23:19:35 -0400

    Fix Cloze card Extra field issue

--- org-anki.el
+++ org-anki.el
@@ -237,7 +237,7 @@ with result."
 
       (cond
        ;; title or content is Cloze: create a Cloze
-       ((org-anki--is-cloze title) `("Cloze" "Text" ,title))
+       ((org-anki--is-cloze title) `("Cloze" "Text" ,title "Extra" ,content))
        ((org-anki--is-cloze content) `("Cloze" "Text" ,content))
        ;; no fields are found in subheadings: take entry title and content
        ((= found-length 0)
