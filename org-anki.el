7067d9ba9e92e5036ac9f8e8a2b579a87de770ec
Bernd Grobauer <bernd.grobauer@krakel.de>
2025-03-11 07:24:07 +0100

    Add org-anki-html-to-org as customizable function
    Also add `org-anki--html-to-org-via-tempfile` to customize it to for
    non-bash shells.

--- org-anki.el
+++ org-anki.el
@@ -151,6 +151,11 @@ very slow."
            (const :tag "Upload file via AnkiConnect's HTTP API" 'http))
   :group 'org-anki)
 
+(defcustom org-anki-html-to-org 'org-anki--html-to-org
+  "Function used to convert HTML (string) to org syntax (string)."
+  :type '(function)
+  :group 'org-anki)
+
 ;; Stolen code
 
 ;; Get list of global properties
@@ -895,7 +900,7 @@ syntax."
           (format
            "%s"
            (org-anki--get-json-field-value (intern it) fields-json))))
-    (cons it (org-anki--html-to-org value-html)))
+    (cons it (funcall org-anki-html-to-org value-html)))
    model-fields))
 
 (defun org-anki--parse-note (note-json deck-name)
@@ -919,6 +924,18 @@ syntax."
        "\n+$" ""
        (shell-command-to-string
         (format "pandoc --wrap=none --from=html --to=org <<< %s" (shell-quote-argument html))))
+    ""))
+
+(defun org-anki--html-to-org-via-tempfile (html)
+  (if html
+      (replace-regexp-in-string
+       "\n+$" ""
+       (let ((temp-file (make-temp-file "org-anki-html-" nil ".html")))
+         (with-temp-file temp-file
+           (insert html))
+         (prog1
+             (shell-command-to-string (format "pandoc --wrap=none --from=html --to=org -i %s" temp-file))
+           (delete-file temp-file))))
     ""))
 
 (defun org-anki--write-note-properties (note)
